a12e6da1bc6fc1dbe4dce1a82569a092698606c9
lizz <lizzp0110@gmail.com>
2022-12-04 23:15:54 -0500

    Se agregaron archivos de SpringBoot en la parte de BackEnd

---
+++ BackEnd/MisHuellitas/src/main/java/com/misiontic/MisHuellitas/ControllerInicio.java
@@ -0,0 +1,16 @@
+package com.misiontic.MisHuellitas;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@Slf4j
+public class ControllerInicio {
+    @GetMapping("/")
+    public String Inicio(){
+        return "index";
+    }
+}
+
---
+++ BackEnd/MisHuellitas/src/test/java/com/misiontic/MisHuellitas/MisHuellitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.MisHuellitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisHuellitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
